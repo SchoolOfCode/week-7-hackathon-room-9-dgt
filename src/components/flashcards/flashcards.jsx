8676304c1263828be9eb6d9bff682e900741d3f5
Timothy Li <timothy93li@gmail.com>
2025-01-23 17:10:38 +0000

    MVP 1 done, next steps: CSS and refactoring

--- src/components/flashcards/flashcards.jsx
+++ src/components/flashcards/flashcards.jsx
@@ -1,25 +1,48 @@
 import Flashcard from "../flashcard/flashcard";
+import { useState } from "react";
 
 function Flashcards() {
-    // const [cards, setCards] = useState([ 
-    //     {
-    //         question: "What colour is a carrot?",
-    //         answer: "Orange",
-    //         id: 1
-    //     }
-    // ])
+  const [cards, setCards] = useState([
+    {
+      question: "What colour is a carrot?",
+      answer: "Orange",
+      id: 1,
+    },
+    {
+      question: "What colour is a banana?",
+      answer: "Yellow",
+      id: 2,
+    },
+  ]);
 
-    console.log("i'm here")
+  console.log("i'm here");
 
-    return <Flashcard card={
-        {
-            question: "What colour is a carrot?",
-            answer: "Orange",
-            id: 1
+  return (
+    <>
+      <Flashcard
+        card={
+          cards[0]
+          // {
+          //     question: "What colour is a carrot?",
+          //     answer: "Orange",
+          //     id: 1
+          // }
         }
-    } />
+      />
+      <Flashcard
+        card={
+          cards[1]
+          // {
+          //     question: "What colour is a carrot?",
+          //     answer: "Orange",
+          //     id: 1
+          // }
+        }
+      />
+    </>
+  );
 }
 
 export default Flashcards;
 
-//const flashcardQuestions = 
+//const flashcardQuestions =
