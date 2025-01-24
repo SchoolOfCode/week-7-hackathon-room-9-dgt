0d4af9272e7d0fcd852281a414b121d42a702dd6
Timothy Li <timothy93li@gmail.com>
2025-01-24 09:16:41 +0000

    dynamic classNames added to the the button that changes colour based on the state of flashcard

--- src/components/flashcard/flashcard.jsx
+++ src/components/flashcard/flashcard.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react';
-import './flashcard.css';
+import { useState } from "react";
+import "./flashcard.css";
 
 function Flashcard({ card }) {
-    //Track a flipped and unflipped state of the card
-    const [flipped, setFlipped] = useState(false);
-    //return button
-    //on click we want the card to change state from unflipped to flipped
-    //Create handleFlip function
-    function handleFlip() {
-        setFlipped(!flipped)
-    }
-    //initial state of flipped will be false 
-    // content of button/card: Question
-    // flipped ? card.answer : card.question
-    // props: card
-    return <button onClick={handleFlip} className='flashcard'>{flipped ? card.answer : card.question} </button>
+  //Track a flipped and unflipped state of the card
+  const [flipped, setFlipped] = useState(false);
+  //return button
+  //on click we want the card to change state from unflipped to flipped
+  //Create handleFlip function
+  function handleFlip() {
+    setFlipped(!flipped);
+  }
+  //initial state of flipped will be false
+  // content of button/card: Question
+  // flipped ? card.answer : card.question
+  // props: card
+  return (
+    <button
+      onClick={handleFlip}
+      className={`flashcard ${flipped ? "answer" : "question"}`} // template literal needed for passing a static and dynamic class name
+    >
+      <div className={flipped ? "answer" : "question"}>
+        {flipped ? card.answer : card.question}
+      </div>
+    </button>
+  );
 }
 export default Flashcard;
